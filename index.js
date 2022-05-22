7bc3b17b445271d11a58faca7ffc05fbf4f27437
dennismacc <dennis.maccarthy@gmail.com>
2022-05-21 21:03:11 -0700

    add info and comments to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const fs = require('fs');
+const { inherits } = require('util');
+
+// Function to create README file
+function createReadme(filename, data) {
+
+}
+
+// Array of questions for user input
+const questions = [
+
+];
+
+// Function to initialize application
+init();
