f42a745bc15ce5057bb2fbc8dc729be88f2377b0
dennismacc <dennis.maccarthy@gmail.com>
2022-05-21 22:15:25 -0700

    add questions array

--- index.js
+++ index.js
@@ -9,7 +9,38 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title for your project?',
-    }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description for your project?',
+    },
+    {
+        type: 'input',
+        name: 'contents',
+        message: 'What would you like to add to your project Table of Contents?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation steps for your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Do you have any usage information?',   
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Would you like to include a license file for your project?',
+        choices: ['Apache License 2.0', 'GNU General Public License', 'MIT License'],
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        messages: 'Would you like to include a list of contributors?',
+    },
 ]
 
 // Function to create README file
@@ -23,11 +54,11 @@ inquirer
         createReadme(answers)
         console.log(chalk.green('Successfully created README.md'))
     })
-    .catch((err) => {
-        if (err.isTtyError) {
+    .catch((error) => {
+        if (error.isTtyError) {
             console.error(`Prompts could not be created`)
         } else {
-            console.error(`Something went wrong`, err)
+            console.error(`Something went wrong`, error)
         }
     });
 
