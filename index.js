edb617092ea262a89e96f171ce6f621e2086b6e0
dennismacc <dennis.maccarthy@gmail.com>
2022-06-20 23:47:31 -0700

    final updates 002

--- index.js
+++ index.js
@@ -51,56 +51,56 @@ const questions = [
 // Function to return license badge
 function licenseBadge(value) {
     if (value === "GNU GPLv3") {
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
     } else if (value === "MPL 2.0") {
-      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
     } else if (value === "MIT") {
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     } else if (value === "Apache 2.0") {
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     } else if (value === "BSD 3-Clause") {
-        return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"; 
+        return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
     } else {
-      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+        return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
     }
-  }
-  
+}
+
 
 // Function to create README file
-function createReadme(answers) { 
+function createReadme(answers) {
     answers.licenseBadge = licenseBadge(answers.license);
     fs.writeFileSync('./READMEexample.md',
-     `
-    # ${answers.title}
-    ${answers.licenseBadge}
+`# ${answers.title}
 
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Contact](#contact)
+${answers.licenseBadge}
+
+## Table of Contents
+ - [Description](#description)
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [License](#license)
+ - [Contributing](#contributing)
+ - [Contact](#contact)
     
-    ## Description
-    ${answers.description}
+## Description
+ ${answers.description}
 
-    ## Installation
-    ${answers.installation}
+## Installation
+ ${answers.installation}
 
-    ## Usage
-    ${answers.usage}
+## Usage
+ ${answers.usage}
 
-    ## License
-    This project is licensed under ${answers.license}
+## License
+ This project is licensed under ${answers.license}
 
-    ## Contributing
-    ${answers.contributing}
+## Contributing
+ ${answers.contributing}
 
-    ## Contact
-    If you have any questions about this project please reach out for more information!
-    - GitHub: https//:github.com/${answers.username}
-    - Email: ${answers.email}
+## Contact
+ If you have any questions about this project please reach out for more information!
+  - GitHub: https//:github.com/${answers.username}
+  - Email: ${answers.email}
     `
     );
 };
